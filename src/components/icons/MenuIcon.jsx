87c591778682b9656a698c0e12ae3318c95c741b
dementus <loai.misto@gmail.com>
2026-04-25 06:36:21 +0300

    feat: add MenuIcon hand-rolled SVG primitive (gv-fcab3a56)

---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MenuIcon({ size = 24, className, style, ...rest }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      focusable="false"
+      aria-hidden="true"
+      className={className}
+      style={{ display: 'inline-block', flexShrink: 0, ...style }}
+      {...rest}
+    >
+      <path d="M3 6h18" />
+      <path d="M3 12h18" />
+      <path d="M3 18h18" />
+    </svg>
+  );
+}
+
+MenuIcon.propTypes = {
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  className: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default MenuIcon;
