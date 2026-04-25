7264f5bd4421dd7efd36702df28bbb93302c07cd
dag <loai.misto@gmail.com>
2026-04-25 06:22:46 +0300

    fix: auto-save uncommitted implementation work (gt-pvx safety net)

--- bin/generate-graphviz-version.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-import { Graphviz } from "@hpcc-js/wasm/graphviz";
-
-const graphviz = await Graphviz.load();
-const graphvizVersion = graphviz.version();
-
-console.log(`const graphvizVersion = "${graphvizVersion}";`);
-console.log('export {graphvizVersion};');
