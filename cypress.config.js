40dcf809fc94bbc32d8090cca0a62f579be0bae4
lmist <loai.misto@gmail.com>
2026-04-29 01:49:07 +0300

    ci: deploy railway and harden dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'cypress';
-import configCodeCoverage from '@cypress/code-coverage/task.js'
 
 export default defineConfig({
   projectId: '8wm34o',
   defaultCommandTimeout: 10000,
   video: true,
+  allowCypressEnv: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      configCodeCoverage(on, config);
-
       return config;
     },
     excludeSpecPattern: '*~',
